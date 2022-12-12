e79bf2929a837ad1125905035882a66426cbfb26
Myoung9300 <myoung9300@icloud.com>
2022-12-12 15:29:10 -0700

    updated cal, sched, and build numbers (12/12)

--- infrastructure/app_flow/tab/schedule/calendar.js
+++ infrastructure/app_flow/tab/schedule/calendar.js
@@ -1,17 +1,10 @@
 import React from "react";
-import { SafeAreaView, ActivityIndicator } from "react-native";
-// import PdfReader from "rn-pdf-reader-js";
+
+import Pdf from "../../../../components/Pdf";
 
 const CalendarScreen = () => {
-  return (
-    <SafeAreaView style={{ flex: 1 }}>
-      {/* <PdfReader
-        source={{ uri: "https://ema-planner.herokuapp.com/Calendar.pdf" }}
-        startInLoadingState={true}
-        renderLoading={() => <ActivityIndicator color="blue" size="large" />}
-      /> */}
-    </SafeAreaView>
-  );
+  const uri = "Calendar";
+  return <Pdf uri={uri} />;
 };
 
 export default CalendarScreen;
--- infrastructure/app_flow/tab/schedule/weeklySchedule.js
+++ infrastructure/app_flow/tab/schedule/weeklySchedule.js
@@ -1,17 +1,10 @@
 import React from "react";
-import { SafeAreaView, ActivityIndicator } from "react-native";
-// import PdfReader from "rn-pdf-reader-js";
+
+import Pdf from "../../../../components/Pdf";
 
 const WeeklySchedule = () => {
-  return (
-    <SafeAreaView style={{ flex: 1 }}>
-      {/* <PdfReader
-        source={{ uri: "https://ema-planner.herokuapp.com/schedule.pdf" }}
-        startInLoadingState={true}
-        renderLoading={() => <ActivityIndicator color="blue" size="large" />}
-      /> */}
-    </SafeAreaView>
-  );
+  const uri = "schedule";
+  return <Pdf uri={uri} />;
 };
 
 export default WeeklySchedule;
